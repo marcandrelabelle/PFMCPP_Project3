f488ac4abda212aae3c42ecd2d55d6d2bde7eecb
matkatmusic <matkatmusic@gmail.com>
2020-07-27 20:30:53 -0700

    added part 1b

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /*
-Project 3 - Part 1a / 5
+Project 3 - Part 1b / 5
 Video:  Chapter 2 Part 5
 User-Defined Types
 
-Create a branch named Part1
+Continue your work on branch Part1
 
 Purpose: The entire purpose of this 5-part project is to get you writing C++ code that compiles and to 
 reinforce the syntax habits that C++ requires.  
@@ -20,67 +20,96 @@ You are going to write 10 UDTs in project3.
     Part 1e: you will convert those 10 plain-english UDTs into code that runs.
 ************************
 
-1) Look at the picture of the car interior (Part1a pic.jpg).  
-    Fill in the blanks below which break this car interior down into sub-objects.
-
-    Several sub-objects are listed below that make up this car's interior.
-        you're going to name several things that you'll find on each subobject
-        you're going to name several things that each subobject can do.
-        If you've seen "Family Feud", we are going to do what they do in that show
-
-        A few blanks are filled in for you already.
-
-Main Object: Car Interior
-Sub Object 1: Steering Wheel
-    Name 4 things you'll find on the:    Steering Wheel
-        1) paddle shifters
-        2) 'cruise control' controls
-        3) car horn
-        4) turn signal switch
-    Name 2 things you can do with the:   Steering Wheel
-        1) adjust cruise control settings.
-        2) turn left or right 
-        
-Sub Object 2: Instrument Cluster
-    Name 4 things you'll find on the:   Instrument Cluster
-        1) Speedometer 
-        2) trip computer 
-        3) tachometer
-        4) indicator lights
-    Name 3 things you can do with the:   Instrument Cluster
-        1) see how much fuel is left in the fuel tank
-        2) display RPM
-        3) display fuel economy
-    
-Sub Object 3: Environment Controls
-    Name 3 things you'll find on the:    Environment Controls
-        1) climate control
-        2) Hazard button 
-        3) air vent
-    Name 3 things you can do with the:   Environment Controls
-        1) turn on/off air conditioning
-        2) turn on/off hazard lights
-        3) change air vent speed
-
-Sub Object 4: Infotainment System
-    Name 3 things you'll find on the:    Infotainment System
-        1) Multi-standard radio
-        2) Smartphone pairing
-        3) compass directions
-    Name 3 things you can do with the:   Infotainment System
-        1) turn on/off radio
-        2) choose between satellite or AM/FM radio
-        3) set the driver or passenger air temperature in fahrenheit 
-
-Sub Object 5: Seat 
-    Name 3 things you'll find on the:    Seat
-        1) seat cover 
-        2) headrest
-        3) rocker switch for adjustment
-    Name 2 things you can do with the:   Seat
-        1) moving the seat forwards and backwards with rocker switch
-        2) ajust headrest height 
+1)  Fill in #1 - 4 with a random UDT in plain english
+    These 4 UDTs do not need to be related.
+        a) For each plain-english UDT, write out 5 traits or properties and 3 things it can do
+        b) remember: these properties will eventually become primitives
 */
+
+/*
+ example:  
+ 
+Thing: Car Wash   
+    5 properties:
+        - number of vacuum cleaners
+        - number of eco-friendly cleaning supplies
+        - stores the amount of water used per week.
+        - stores amount of profit made per week
+        - number of cars serviced per day
+    3 things it can do:
+        - wash and wax car
+        - charge customer
+        - detail the car interior
+
+    Notice that I did not use "has a vacuum cleaner" or "Has eco-friendly cleaning supplies" as one of the properties
+    
+    Writing 'has a ___" checks whether or not your object **has the ability to do something**.
+    Instead, I wrote "number of vacuum cleaners" and "number of eco-friendly cleaning supplies".  
+    
+    These are specific objects or amounts. 
+    
+    In C++ terms, this means to I want you to avoid using 'bool' (has a) as a member variable type.
+    Instead, prefer the other primitive types.
+ */
+
+/*
+Thing 1)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+/*
+Thing 2)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+/*
+Thing 3)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+/*
+Thing 4)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+
+
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
