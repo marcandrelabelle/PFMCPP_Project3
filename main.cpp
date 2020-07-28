884d2243588cfc3ae4589eeed6db05856f0debd2
marclab <marc_lab2091@hotmail.com>
2020-07-28 02:18:09 +0000

    Part 1a is completed

--- main.cpp
+++ main.cpp
@@ -35,51 +35,51 @@ Sub Object 1: Steering Wheel
     Name 4 things you'll find on the:    Steering Wheel
         1) paddle shifters
         2) 'cruise control' controls
-        3) 
-        4) 
+        3) car horn
+        4) turn signal switch
     Name 2 things you can do with the:   Steering Wheel
         1) adjust cruise control settings.
-        2)
+        2) turn left or right 
         
 Sub Object 2: Instrument Cluster
     Name 4 things you'll find on the:   Instrument Cluster
-        1)
-        2)
-        3)
-        4)
+        1) Speedometer 
+        2) trip computer 
+        3) tachometer
+        4) indicator lights
     Name 3 things you can do with the:   Instrument Cluster
-        1)
-        2)
-        3)
+        1) see how much fuel is left in the fuel tank
+        2) display RPM
+        3) display fuel economy
     
 Sub Object 3: Environment Controls
     Name 3 things you'll find on the:    Environment Controls
-        1)
-        2)
-        3)
+        1) climate control
+        2) Hazard button 
+        3) air vent
     Name 3 things you can do with the:   Environment Controls
-        1)
-        2)
-        3)
+        1) turn on/off air conditioning
+        2) turn on/off hazard lights
+        3) change air vent speed
 
 Sub Object 4: Infotainment System
     Name 3 things you'll find on the:    Infotainment System
-        1)
-        2)
-        3)
+        1) Multi-standard radio
+        2) Smartphone pairing
+        3) compass directions
     Name 3 things you can do with the:   Infotainment System
-        1)
-        2)
-        3)
+        1) turn on/off radio
+        2) choose between satellite or AM/FM radio
+        3) set the driver or passenger air temperature in fahrenheit 
 
 Sub Object 5: Seat 
     Name 3 things you'll find on the:    Seat
-        1)
-        2)
-        3)
+        1) seat cover 
+        2) headrest
+        3) rocker switch for adjustment
     Name 2 things you can do with the:   Seat
-        1)
-        2)
+        1) moving the seat forwards and backwards with rocker switch
+        2) ajust headrest height 
 */
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
